250d7743dcf2d85e39620d468fe5264051815a4c
liuyaowen <liuyaowen_smile@126.com>
2024-07-23 15:38:26 +0800

    集成 monaco editor

---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import {ref, shallowRef, toRaw, watch} from 'vue'
+import {VueMonacoEditor} from '@guolao/vue-monaco-editor'
+import * as monaco from 'monaco-editor'
+
+const MONACO_EDITOR_OPTIONS = {
+  automaticLayout: true,
+  formatOnType: true,
+  formatOnPaste: true,
+}
+
+const code = defineModel('code', {
+  type: String,
+  default: '',
+})
+
+
+const editorRef = shallowRef()
+const handleMount = (editor: any) => {
+  editorRef.value = editor
+}
+
+
+
+</script>
+<template>
+  <div id="code-editor">
+    <VueMonacoEditor
+        v-model:value="code"
+        language="java"
+        theme="vs-dark"
+        :options="MONACO_EDITOR_OPTIONS"
+        @mount="handleMount"
+        height="100%"
+    />
+  </div>
+</template>
+<style scoped>
+#code-editor {
+  height: 500px;
+}
+</style>
