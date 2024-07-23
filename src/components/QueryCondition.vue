882290ac208d005cc497c6cb2ce844e1c2f8ba2c
liuyaowen <liuyaowen_smile@126.com>
2024-07-23 15:42:38 +0800

    新增 题目展示页面

---
+++ src/components/QueryCondition.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import {useRouter} from 'vue-router'
+import {ref} from "vue";
+const router = useRouter()
+
+const onFilter = () => {
+  console.log('filter',condition)
+  emit('filter', condition.value)
+}
+const condition = ref({
+  title: '',
+  tags: Array<string>()
+})
+
+const emit = defineEmits(['filter'])
+
+</script>
+
+<template>
+
+  <div id="query-condition">
+
+    <a-row id="grid-demo" style="margin-bottom: 16px;">
+      <a-col flex="auto">
+        <a-tag color="blue">题目内容:</a-tag>
+        <a-input v-model="condition.title" :style="{width:'200px'}" placeholder="请输入题目内容"/>
+
+        <a-tag color="blue">题目标签:</a-tag>
+        <a-input-tag v-model="condition.tags" :style="{width:'200px'}" placeholder="请输入标签" allow-clear/>
+
+        <a-button @click="onFilter" type="outline">查询</a-button>
+      </a-col>
+      <a-col flex="100px">
+        <a-button type="primary" @click="router.push('/question/add')">新增</a-button>
+      </a-col>
+    </a-row>
+  </div>
+
+</template>
+
+<style scoped>
+#grid-demo {
+  padding: 8px;
+  background: #f0f2f5;
+
+}
+
+</style>
