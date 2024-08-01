0123d1cc88b2c8edf7f09d97094d24be07e7f378
liuyaowen <liuyaowen_smile@126.com>
2024-08-01 19:35:35 +0800

    加入打包可视化 优化打包体积

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from "@vitejs/plugin-vue";
+import {visualizer} from "rollup-plugin-visualizer";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    visualizer({
+      open: true,
+      emitFile: false,
+      filename: 'stats.html',
+      sourcemap: false,
+    }),
   ],
   resolve: {
     alias: {
